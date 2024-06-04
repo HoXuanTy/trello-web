7a5de14d69478fc9d39cbbd3e5ef845ccc5c16bb
Ho Xuan Ty <hoxuanty2003qng@gmail.com>
2024-06-04 11:37:01 +0700

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import Button from '@mui/material/Button'
 
 function App() {
   return (
-    <>
+    <div>
       <p>Dev T</p>
       <Button variant="contained">Hello world</Button>
-    </>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline/>
     <App />
