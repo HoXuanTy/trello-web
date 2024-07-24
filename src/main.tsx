5c725b31a508797dd47a4f7184f7f96db9b17b16
Ho Xuan Ty <hoxuanty2003qng@gmail.com>
2024-07-24 16:52:58 +0700

    feat: add flash message

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
+      <ToastContainer theme="colored" position="bottom-right" />
     </CssVarsProvider>
   </React.StrictMode>
 );
