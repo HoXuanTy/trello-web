aff053c00be99b6e532a11c34f6823cbfcb6c1d7
Ho Xuan Ty <hoxuanty2003qng@gmail.com>
2024-08-29 09:53:29 +0700

    feat: add index page

--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,16 @@ import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/s
 import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
+import Index from "./pages/IndexPage/Index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <Provider store={store}>
-        <App />
-      </Provider>
-      <ToastContainer theme="colored" position="bottom-right" />
-    </CssVarsProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<CssVarsProvider theme={theme}>
+			<CssBaseline />
+			<Provider store={store}>
+				<App />
+			</Provider>
+			<ToastContainer theme="colored" position="bottom-right" />
+		</CssVarsProvider>
+	</React.StrictMode>
 );
